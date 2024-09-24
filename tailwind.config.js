27dd7141e7136d51aee8dc63500826148b850f70
chidera nwokoye <chideranwokoye555@gmail.com>
2024-09-24 16:09:44 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,9 @@ module.exports = {
     extend: {
       backgroundImage: {
         background: "url(./image/pattern.svg)",
-        "parkingimage": "url(./image/parking.svg)",
-        "colour-1": "linear-gradient(90deg, #F2F32D 0%, #FECBD9 100%)",
+        parkingimage: "url(./image/parking.svg)",
+        "colour-2": "linear-gradient(180deg, #065D44 0%, #023627 100%)",
+"colour-1": "linear-gradient(90deg, #F2F32D 0%, #FECBD9 100%)",
         colours: "linear-gradient(180deg, #42CD6B 0%, #088E3F 100%)",
       },
       colors: {
